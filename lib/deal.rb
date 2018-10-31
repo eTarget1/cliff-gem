574a1e14a94cf4b820cd1c9e06cec2c9d290a087
clifford <clifford@noukod.com>
2018-10-31 15:13:07 +0000

    c

--- lib/deal.rb
+++ lib/deal.rb
@@ -1,5 +1,5 @@
 class CliffGem::Deal
-  attr_accessor :name, :price, :avaibility, :url,
+  attr_accessor :name, :price, :avaibility, :url, :color
 
   def self.today
     # puts "this workd now!"
@@ -10,7 +10,7 @@ class CliffGem::Deal
   def self.scrape_deals
     deals = []
     deals << self.scrape_cam
-    #deals << self.scrape_headset
+    deals << self.scrape_headset
 
     deals
     # deal_1 = self.new
@@ -33,9 +33,23 @@ class CliffGem::Deal
 
   def self.scrape_cam
      doc = Nokogiri::HTML(open("https://www.amazon.co.uk/dp/B01NAKVPDJ/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B01NAKVPDJ&pf_rd_m=A3P5ROKL5A1OLE&pf_rd_p=8e5be0e3-b258-4b06-8b6e-695b24f935a4&pf_rd_r=G1DJ38QMFG7W431TSCH0&pd_rd_wg=G8iCC&pf_rd_s=desktop-dp-sims&pf_rd_t=40701&pd_rd_w=lOZP1&pf_rd_i=desktop-dp-sims&pd_rd_r=b4e0d20b-d861-11e8-be6c-5d68b767c2a7"))
-
-     name = doc.css("#productTitle").text.strip
-     price = doc.css("#price_inside_buybox").text.strip
-     binding.pry
+     deal = self.new
+     deal.name = doc.css("#productTitle").text.strip
+     deal.price = doc.css("#price_inside_buybox").text.strip
+     deal.avaibility = true
+     deal
+     #binding.pry
    end
+
+   def self.scrape_headset
+     doc = Nokogiri::HTML(open("https://www.amazon.co.uk/Mpow-Headset-Multi-Use-headset-Reduction/dp/B06XX7SYP2"))
+     deal = self.new
+     deal.name = doc.css("#productTitle")
+      deal.price = doc.css("#price_inside_buybox").text.strip
+      deal.color = doc.css("#selection").text.strip
+      deal.avaibility = true
+
+      deal
+   end
+
 end
